e1f7472570f29be73707a2f3fdae803a5dbe557f
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2024-11-08 03:36:19 +0000

    Found a bug YEAH!

--- test_calculator.py
+++ test_calculator.py
@@ -7,8 +7,10 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
-
+        self.assertEqual(self.calc.add(-9, -5), -14)
+        self.assertEqual(self.calc.add(-5, 8), 3)
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(-10, 20), -30)
     # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
